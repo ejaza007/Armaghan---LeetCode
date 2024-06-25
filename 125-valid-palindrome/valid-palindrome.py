6a1354ba66c73372308f1317416807ea2233df43
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-06-25 17:37:43 +0500

    Time: 50 ms (29.82%) | Memory: 21.6 MB (13.10%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Filter out non-alphanumeric characters and convert to lowercase
+        filtered_s = ''.join(char.lower() for char in s if char.isalnum())
+        
+        l, r = 0, len(filtered_s) - 1
+        
+        while l < r:
+            if filtered_s[l] == filtered_s[r]:
+                l += 1
+                r -= 1
+            else:
+                return False
+        return True
