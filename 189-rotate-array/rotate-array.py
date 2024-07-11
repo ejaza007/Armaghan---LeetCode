e25e1b1a0d2c57a55b80a15784ba9577312b5109
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-07-11 17:42:45 +0500

    Time: 138 ms (99.17%) | Memory: 28 MB (19.10%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,21 +3,21 @@ class Solution:
         k = k % len(nums)
         l,r = 0,len(nums) - k - 1
 
-        while l<=r:
+        while l<r:
             nums[l], nums[r] = nums[r], nums[l]
             l+=1
             r-=1
 
         l,r = len(nums) - k , len(nums) - 1 
 
-        while l<=r:
+        while l<r:
             nums[l], nums[r] = nums[r], nums[l]
             l+=1
             r-=1
         
         l,r = 0, len(nums) - 1
 
-        while l<=r :
+        while l<r :
             nums[l], nums[r] = nums[r], nums[l]
             l+=1
             r-=1
