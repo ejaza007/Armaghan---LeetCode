dd4f388871f698219abaed11296cb2ceeedc19b0
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-07-23 15:05:28 +0500

    Time: 43 ms (44.69%) | Memory: 16.8 MB (87.60%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        res = 0
+
+        for i in range(len(citations)):
+            if i + 1 <= citations[i]:
+                res = i + 1
+            else:
+                break
+                
+        return res
+
+            
+
+
+
+
+        
