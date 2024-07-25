5530acd9264ee1fdb8d867015f578de1446c786a
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-07-25 18:12:56 +0500

    Time: 57 ms (93.60%) | Memory: 17 MB (54.31%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -7,19 +7,19 @@ class Solution:
 
         for s in tokens:
             if s not in arithmeticset:
-                stack.append(int(s))  # Convert string to integer and append
+                stack.append(int(s))  
             else:
-                num1 = stack.pop()  # Operand 1
-                num2 = stack.pop()  # Operand 2
+                num1 = stack.pop()  
+                num2 = stack.pop()  
 
                 if s == "+":
-                    stack.append(num2 + num1)  # Correct addition
+                    stack.append(num2 + num1)  
                 elif s == "-":
-                    stack.append(num2 - num1)  # Correct subtraction
+                    stack.append(num2 - num1)  
                 elif s == "/":
-                    stack.append(int(num2 / num1))  # Correct integer division
+                    stack.append(int(num2 / num1)) 
                 elif s == "*":
-                    stack.append(num2 * num1)  # Correct multiplication
+                    stack.append(num2 * num1) 
 
-        return stack.pop()  # The result should be the last remaining element
+        return stack.pop() 
 
