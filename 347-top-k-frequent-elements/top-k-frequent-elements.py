aed72666e45f3814b96e38bf3ed418e4e69463c1
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-08-25 00:30:11 +0500

    Time: 73 ms (99.16%) | Memory: 21.1 MB (65.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,27 +1,20 @@
-from typing import List
-import heapq
-from collections import Counter
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # Step 1: Count the frequency of each element
-        frequency = Counter(nums)
-        
-        # Step 2: Use a min-heap to keep track of top k elements
-        min_heap = []
-        
-        for num, freq in frequency.items():
-            heapq.heappush(min_heap, (freq, num))
-            if len(min_heap) > k:
-                heapq.heappop(min_heap)
-        
-        # Step 3: Extract the elements from the heap
-        top_k_frequent = [num for freq, num in min_heap]
-        
-        return top_k_frequent
+        counted = Counter(nums)
 
-# Example usage:
-solution = Solution()
-nums = [1,1,1,2,2,3]
-k = 2
-print(solution.topKFrequent(nums, k))  # Output: [1, 2]
+        heap = []
+
+        for num, freq in counted.items():
+            heapq.heappush(heap, (freq, num))
+            
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        res = []
+
+        for freq,num in heap:
+            res.append(num)
+
+        return res
+
+        
