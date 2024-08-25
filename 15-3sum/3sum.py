eaca2562aafa4eb43e8a2781d1c9b1109cc46659
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-08-25 19:09:33 -0400

    Time: 717 ms (46.44%) | Memory: 20.7 MB (70.66%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,40 +1,32 @@
+from typing import List
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-
-        res = []
         nums.sort()
-
+        res = []
 
         for i in range(len(nums)):
-
             if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            l,r = i + 1, len(nums) - 1
+            l, r = i + 1, len(nums) - 1
 
             while l < r:
+                current_sum = nums[i] + nums[l] + nums[r]
 
-                if nums[i] + nums[l] + nums[r] == 0:
+                if current_sum > 0:
+                    r -= 1
+                elif current_sum < 0:
+                    l += 1
+                else:
                     res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
 
-                    while l<r and nums[l] == nums[l+1]:
-                        l+=1
-                    while r>l and nums[r] == nums[r-1]:
-                        r-=1
-
-                    l+=1
-                    r-=1
-
-                elif nums[i] + nums[l] + nums[r] > 0:
-                    r-=1
-                elif nums[i] + nums[l] + nums[r] < 0:
-                    l+=1
+                    # Skip duplicates after finding a valid triplet
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
 
         return res
-                
-
-
-
-
-
-        
