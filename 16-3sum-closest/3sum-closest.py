038afeed395cacd5fe9097521a4e363ca0a93aec
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-09-07 18:18:27 -0400

    Time: 377 ms (75.92%) | Memory: 16.6 MB (77.39%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -6,10 +6,10 @@ class Solution:
         closest = float('inf')
         res = 0
 
-        for i in range(len(nums) - 2):  # Ensure there are at least two more elements after nums[i]
+        for i in range(len(nums)):  # Ensure there are at least two more elements after nums[i]
             l, r = i + 1, len(nums) - 1
 
-            while l < r:
+            while l <r:
                 threesum = nums[i] + nums[l] + nums[r]
 
                 if abs(threesum - target) < abs(closest):
