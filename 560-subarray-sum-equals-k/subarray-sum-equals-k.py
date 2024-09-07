c2c83e5ab8f351887144af7e71507d6b1bc1ff11
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-09-07 18:15:32 -0400

    Time: 226 ms (68.55%) | Memory: 19.6 MB (19.41%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,20 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         res = 0
-        countMap = {0: 1}  # Initialize with 0:1 to handle subarrays that start from the beginning
-        add = 0
+        summap = {0: 1}  # Initialize with 0:1 to handle subarrays starting from index 0
+        total = 0
 
         for num in nums:
-            add += num
-            if add - k in countMap:
-                res += countMap[add - k]
-            if add in countMap:
-                countMap[add] += 1
-            else:
-                countMap[add] = 1
-                
+            total += num  # Add current num to the running total
+            if total - k in summap:
+                res += summap[total - k]  # Add the count of subarrays that sum to k
+            summap[total] = summap.get(total, 0) + 1  # Update the count of the current total in summap
+
         return res
-
-
 
         
