f9b374164a10d5eac9dce91bc8933c7a211c905d
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-09-08 19:42:19 -0400

    Time: 20 ms (99.58%) | Memory: 16.5 MB (73.50%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,22 +1,24 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
-
         for i in range(len(s)):
             if s[i] != "]":
+                # Push current character to the stack
                 stack.append(s[i])
             else:
+                # Start popping from stack until we find '['
                 substr = ""
                 while stack[-1] != "[":
                     substr = stack.pop() + substr
+                # Remove the '[' from the stack
                 stack.pop()
 
+                # Now, we need to extract the number (multiplier)
                 k = ""
                 while stack and stack[-1].isdigit():
                     k = stack.pop() + k
-                stack.append(int(k)* substr)
-        return "".join(stack)
-
-
-
+                # Repeat the substring k times and push back to stack
+                stack.append(int(k) * substr)
         
+        # Finally, join everything in the stack and return the result
+        return "".join(stack)
