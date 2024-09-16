bc23acb9d91e5e3eebcb91736820412f3ecba292
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-09-15 22:41:46 -0400

    Time: 82 ms (88.79%) | Memory: 20 MB (21.13%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,29 +1,33 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # Remove all spaces from the string
-        s = s.replace(" ", "")
+        if not s:
+            return 0
+
         stack = []
-        current_number = 0
-        last_operator = '+'
-        
-        for i, c in enumerate(s):
-            if c.isdigit():
-                current_number = current_number * 10 + int(c)
-                
-            if c in "+-*/" or i == len(s) - 1:
-                if last_operator == '+':
-                    stack.append(current_number)
-                elif last_operator == '-':
-                    stack.append(-current_number)
-                elif last_operator == '*':
-                    stack.append(stack.pop() * current_number)
-                elif last_operator == '/':
-                    # Handle integer division and truncate toward zero
-                    stack.append(int(stack.pop() / current_number))
-                
-                # Reset current number and update the last operator
-                current_number = 0
-                last_operator = c
-        
-        # The final result is the sum of the stack
+        current_num = 0
+        operation = '+'
+        s += '+'  # Append a '+' to handle the last number
+
+        for char in s:
+            if char.isdigit():
+                current_num = current_num * 10 + int(char)
+            elif char in '+-*/':
+                # Process the previous operation with current_num
+                if operation == '+':
+                    stack.append(current_num)
+                elif operation == '-':
+                    stack.append(-current_num)
+                elif operation == '*':
+                    stack[-1] = stack[-1] * current_num
+                elif operation == '/':
+                    # Integer division truncated towards zero
+                    stack[-1] = int(stack[-1] / current_num)
+
+                # Update operation to the current operator
+                operation = char
+                # Reset current_num for the next number
+                current_num = 0
+
+        # Sum up all numbers in the stack for the result
         return sum(stack)
+
