059861463679cd09b475a30f64cafcb34eaba53e
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-10-22 23:36:45 -0400

    Time: 0 ms (100.00%) | Memory: 16.7 MB (8.00%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -2,26 +2,19 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        l = 0
-        r = len(nums) - 1
+        """
+        Removes all instances of val in nums in-place and returns the new length.
+        The order of elements can be changed. It doesn't matter what is left beyond the new length.
+        """
+        # Pointer to track position of the next element to keep
+        k = 0
 
-        while l <= r:
-            if nums[l] == val:
-                nums[l], nums[r] = nums[r], nums[l]
-                r -= 1
-            else:
-                l += 1
+        # Iterate through all elements in the array
+        for i in range(len(nums)):
+            # If the current element is not equal to val, we keep it
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
 
-        return l
-
-# Test case
-nums = [3, 2, 2, 3]
-val = 3
-solution = Solution()
-length = solution.removeElement(nums, val)
-print(length)  # Output: 2
-print(nums[:length])  # Output: [2, 2]
-
-
-
-        
+        # Return the length of the array after removal
+        return k
