eaba2ed9ba51d43789b1cc6cbe21d9e0dfa4b641
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-10-27 22:52:34 -0400

    Time: 42 ms (31.78%) | Memory: 17.5 MB (92.75%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,10 +1,9 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         maxR = 0
-        for i, length in enumerate(nums):
+        for i in range(len(nums)):
             if i > maxR:
                 return False
-            maxR = max(maxR, i + length)
+            maxR = max(maxR, i + nums[i])
         return True
-
         
