f6d480bc7e102d0c85c14d0bc1c4c0d166158930
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-10-27 22:12:01 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (95.44%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -2,8 +2,11 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
+        max_profit = 0  # Initialize the maximum profit
+        
         for i in range(1, len(prices)):
+            # If today's price is higher than yesterday's price, we can profit
             if prices[i] > prices[i - 1]:
                 max_profit += prices[i] - prices[i - 1]
+        
         return max_profit
