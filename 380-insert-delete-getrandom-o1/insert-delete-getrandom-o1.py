ce15d6eefd09566cd06e85c62092a05a93807b21
Armaghan Ejaz <123661496+ejaza007@users.noreply.github.com>
2024-10-28 00:13:40 -0400

    Time: 88 ms (92.01%) | Memory: 56.3 MB (89.15%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -3,33 +3,34 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.numSet = set()
-        self.numArr = []
+        # Dictionary to store the index of each value for O(1) removal
+        self.val_to_index = {}
+        # List to store the values for O(1) random access
+        self.data = []
 
     def insert(self, val: int) -> bool:
-        if val in self.numSet:
+        # If the value already exists, return False
+        if val in self.val_to_index:
             return False
-        else:
-            self.numSet.add(val)
-            self.numArr.append(val)
-            return True
+        # Otherwise, add the value
+        self.val_to_index[val] = len(self.data)
+        self.data.append(val)
+        return True
 
     def remove(self, val: int) -> bool:
-        if val in self.numSet:
-            self.numSet.remove(val)
-            self.numArr.remove(val)
-            return True
-        else:
+        # If the value does not exist, return False
+        if val not in self.val_to_index:
             return False
+        # Swap the element to be removed with the last element
+        last_element = self.data[-1]
+        idx_to_remove = self.val_to_index[val]
+        self.data[idx_to_remove] = last_element
+        self.val_to_index[last_element] = idx_to_remove
+        # Remove the last element from the list and dictionary
+        self.data.pop()
+        del self.val_to_index[val]
+        return True
 
     def getRandom(self) -> int:
-        return random.choice(self.numArr)
-
-        
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        # Return a random element from the list
+        return random.choice(self.data)
